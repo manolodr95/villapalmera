c13eabd1056b318bbc0ed4703ed4c455f97bfc0c
Manolo <manolodr95@gmail.com>
2025-01-28 22:34:19 -0400

    4th_Commit
    4th_Commit

---
+++ condo/models/__init__.py
@@ -0,0 +1,7 @@
+# © 2024 Daniel Eduardo Diaz Mateo <daniel.diaz@isjo-technology.com>
+
+from . import condo_contract
+from . import account_move
+from . import res_config_settings
+from . import condo_contract_line
+from . import view_report_pay_invoice
