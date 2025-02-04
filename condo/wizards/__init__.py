f1e0bd55f895e1841672e6f4e225e9230d3e4d5d
Manolo <manolodr95@gmail.com>
2025-02-04 13:23:19 -0400

    Delete condo directory

--- condo/wizards/__init__.py
+++
@@ -1,7 +0,0 @@
-# © 2024 Daniel Eduardo Diaz Mateo <daniel.diaz@isjo-technology.com>
-
-from . import condo_contract_line_wizard
-from . import mark_payment
-from . import wizard_late_fee
-from . import condo_charge_report
-from . import account_register_payment
